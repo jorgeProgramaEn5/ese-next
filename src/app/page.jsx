bdcf28c024fc4817e518e13745fac73d02a44847
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-07-01 16:11:27 -0500

    add tarject

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Navbar from '@/src/components/Navbar'
 import Hero from '@/src/components/Hero' 
 import SectionCard from '@/src/components/SectionCard' 
 import Contact from '@/src/components/Contact' 
+import Skills from '@/src/components/Skills' 
 import Footer from '@/src/components/Footer' 
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero/>
       <SectionCard/>
       <Contact/>
+      <Skills/>
       <Footer/>
     </main>
   )
